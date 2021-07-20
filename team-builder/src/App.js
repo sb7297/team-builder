63c068d37cee98f7ba3152d5507814db17f62387
sb7297 <sbaireddy7297@gmail.com>
2021-07-20 11:27:10 -0400

    App: define team members state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,23 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([
+    {name: "Harry Potter", email: "hp102@hogwarts.edu", role: "Wizard"},
+    {name: "Mycroft Canner", email: "anonymous@anonymous.org", role: "Rogue"},
+    {name: "Breq Mianaai", email: "breq@mianaai.gov", role: "Ship"}
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {teamMembers.map(member => {
+      return (<ul>
+        <li>{member.name}</li>
+        <li>{member.email}</li>
+        <li>{member.role}</li>
+      </ul>);
+    })}
     </div>
   );
 }
